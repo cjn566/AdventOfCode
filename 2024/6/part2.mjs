67e0918898a228819e6378d8922e9fe17a64e249
Colten Nye <colten.nye@gmail.com>
2024-12-11 22:34:17 -0800

    finish day 6

--- 2024/6/part2.mjs
+++ 2024/6/part2.mjs
@@ -4,15 +4,11 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const input = fs.readFileSync(path.join(dirname(fileURLToPath(import.meta.url)), 'input.txt'), 'utf8')
 
-let dir = 0, sum = 0, sim = false, first = true, x, y, sx, sy, sd, T, stops = [], startX, startY
-
 const start = performance.now()
-
-let lines = input.split('\n').map((line, my) => {
-    return line.split('').map((char, mx) => {
+let startX, startY
+let baseMap = input.split('\n').map((line, my) => {
+    return line.trim().split('').map((char, mx) => {
         if (char === '^') {
-            x = mx
-            y = my
             startX = mx
             startY = my
         }
@@ -20,86 +16,49 @@ let lines = input.split('\n').map((line, my) => {
     })
 })
 
-function simulate() {
-    sim = true
-    first = true
-    if (dir % 2) {
-        sy = y
-        sx = T
-    } else {
-        sy = T
-        sx = x
+const baseMapStr = JSON.stringify(baseMap)
+
+const H = baseMap.length
+const W = baseMap[0].length
+
+
+function runMap(map, buildPath = false) {
+    let stops = new Map()
+    let steps
+    if(buildPath) steps = new Array(H).fill(null).map(x => new Array(W))
+    let x = startX, y = startY, dir = 0, T
+    while (true) {
+        if (dir % 2) { // Left / Right
+            T = x + ((dir - 1) ? -1 : 1)
+            while (T >= 0 && T < W && !map[y][T]) {
+                if(buildPath) steps[y][T] = true
+                T += (dir - 1) ? -1 : 1
+            }
+            x = T + ((dir - 1) ? 1 : -1)
+        } else { // Up / Down
+            T = y + ((dir ? 1 : -1))
+            while (T >= 0 && T < H && !map[T][x]) {
+                if(buildPath) steps[T][x] = true
+                T += (dir ? 1 : -1)
+            }
+            y = T + (dir ? -1 : 1)
+        }
+        if (T < 0 || T >= ((dir % 2) ? W : H)) {
+            return (buildPath ? steps : false)
+        }
+        if(stops.has(`${y},${x},${dir}`)) return true
+        stops.set(`${y},${x},${dir}`)
+        dir = (dir + 1) % 4
     }
-    stops.push([sy, sx])
-    sd = dir
-    lines[sy][sx] = true
-    // console.log(`sim from y:${y} x:${x} d:${dir % 2 ? dir - 1 ? '<' : '>' : dir ? 'v' : '^'}`)
 }
 
-function endSim(loop) {
-    sum += loop
-    stops = []
-    sim = false
-    x = sx
-    y = sy
-    dir = sd
-    lines[sy][sx] = false
-}
-
-const lastY = lines.length - 1
-const lastX = lines[0].length - 1
-
-while (true) {
-    // if (performance.now() - start > 500) break
-    if (dir % 2) { // Left / Right
-        // (dir-1) == Left
-        T = x
-        while (T >= 0 && T <= lastX && !lines[y][T]) {
-            if (!sim && T !== x) {
-                simulate()
-                break
-            }
-            T += (dir - 1) ? -1 : 1
-            //console.log(`step ${dir - 1 ? '<' : '>'} to y:${y + 1} x:${T + 1}`)
-        }
-        x = T + ((dir - 1) ? 1 : -1)
-    } else { // Up / Down
-        // dir == Down
-        T = y
-        while (T >= 0 && T <= lastY && !lines[T][x]) {
-            if (!sim && T !== y) {
-                simulate()
-                break
-            }
-            T += (dir ? 1 : -1)
-            //console.log(`step ${dir ? 'v' : '^'} to y:${T + 1} x:${x + 1}`)
-        }
-        y = T + (dir ? -1 : 1)
-    }
-    if (T < 0 || T > ((dir % 2) ? lastX : lastY)) {
-        if (sim) {
-            endSim(0)
-            continue
-        }
-        else {
-            break
-        }
-    }
-    if (sim) {
-        if (first) {
-            first = false
-        } else if (stops.filter(stop => stop[0] == y && stop[1] == x).length > 1) {
-            if(sx === startX && sy === startY){
-                console.log('fuck')
-            } else {
-                endSim(1)
-            }
-            continue
-        }
-        stops.push([y, x])
-    }
-    dir = (dir + 1) % 4
-}
+let sum = runMap(baseMap, true).reduce((sum, line, y) => {
+    return sum + line.filter((_, x) => {
+        let tryMap = JSON.parse(baseMapStr)
+        tryMap[y][x] = true
+        return runMap(tryMap)
+    }).length
+}, 0)
 
 const end = performance.now()
 
